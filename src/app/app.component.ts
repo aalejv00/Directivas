bdc7e9d14220c062ab014e7348d8985185a4a4b4
Alba <aalejandre@fundacionsafa.es>
2023-10-19 17:32:17 +0200

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DirecEstr';
+  titulo = 'Registro de Usuarios';
+  mensaje= "";
+  registrado= false;
+  nombre="";
+  apellido="";
+/*  entradas: object=[];*/
+
+
+
+ /*constructor() {
+   this.entradas[
+     {titulo:"Java alcanza la popularidad"},
+     {titulo:"Phyton cada día más presente"},
+     {titulo:"CSS como lenguaje de marca más exitoso"},
+     {titulo:"WordPress se alza con la medalla"},
+   ];
+ }*/
+  registrarUsuario(){
+    this.registrado= true;
+    this.mensaje= "Usuario registrado con éxito";
+  }
 }
